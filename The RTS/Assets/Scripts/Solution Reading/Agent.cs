4ff47bd6b8d0c3a8648493d18d892f92d4f7178a
DrWidget <ope_o_lad@yahoo.co.uk>
2016-05-14 15:42:35 +0100

    Agents
    Added the base of the agents to the actual project itself.

---
+++ The RTS/Assets/Scripts/Solution Reading/Agent.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent
+{
+	public delegate void Event_Mission_Action(params string[] its_actions);
+	public delegate bool Event_Mission_Check(params string[] its_actions);
+
+	private Event_Mission_Action my_mission_start;
+	private Event_Mission_Action my_mission_action;
+	private Event_Mission_Action my_mission_complete;
+
+	private Event_Mission_Check my_mission_check;
+
+
+
+	public string[] my_agent_actions;
+
+	public void Set_Agent(Event_Mission_Action its_start_action,
+	                      Event_Mission_Action its_during_action,
+	                      Event_Mission_Action its_complete_action,
+	                      Event_Mission_Check its_complete_check)
+	{
+		my_mission_start = its_start_action;
+		my_mission_action = its_during_action;
+		my_mission_complete = its_complete_action;
+		my_mission_check = its_complete_check;
+	}
+
+	public void Start_Agent()
+	{
+		AgentMaster.Add_Agent(this);
+	}
+
+	public void Abort_Agent()
+	{
+		AgentMaster.Abort_Agent(this);
+	}
+
+	public void Call_Start()
+	{
+		my_mission_start(my_agent_actions);
+	}
+	
+	public void Call_Update()
+	{
+		my_mission_action(my_agent_actions);
+		if (my_mission_check(my_agent_actions))
+			Abort_Agent();
+	}
+	
+	public void Call_End()
+	{
+		my_mission_complete(my_agent_actions);
+	}
+
+	public Agent()
+	{
+	}
+}
---
+++ The RTS/Assets/Scripts/Solution Reading/AgentMaster.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AgentMaster : MonoBehaviour
+{
+	public static AgentMaster self;
+
+	private List<Agent> my_active_agents;
+
+	//public 
+
+	void Awake()
+	{
+		self = this;
+		my_active_agents = new List<Agent>();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	public static void Add_Agent(Agent its_new_agent)
+	{
+		self.my_active_agents.Add(its_new_agent);
+		its_new_agent.Call_Start();
+	}
+	
+	public static void Abort_Agent(Agent its_agent)
+	{
+		self.my_active_agents.Remove(its_agent);
+		its_agent.Call_End();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (Agent rep_agent in my_active_agents)
+		{
+			rep_agent.Call_Update();
+		}
+	}
+
+	public string Read_Solution(string its_filename)
+	{
+
+	}
+}
